32af66f91223d83b45b1eb49f1abf2a7d15662bd
werunm <cankennethwong@gmail.com>
2023-02-27 11:10:18 -0500

    feat: made changes to be compatible with python3.9

--- data_loader.py
+++ data_loader.py
@@ -6,12 +6,19 @@ def get_loader(config):
     """Builds and returns Dataloader for MNIST and SVHN dataset."""
     
     transform = transforms.Compose([
-                    transforms.Scale(config.image_size),
+                    transforms.Resize(config.image_size),
                     transforms.ToTensor(),
                     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
     
+
+    transform_mnist = transforms.Compose([
+                    transforms.Resize(config.image_size),
+                    transforms.ToTensor(),
+                    transforms.Normalize((0.5,), (0.5,))
+                    ])
+    
     svhn = datasets.SVHN(root=config.svhn_path, download=True, transform=transform)
-    mnist = datasets.MNIST(root=config.mnist_path, download=True, transform=transform)
+    mnist = datasets.MNIST(root=config.mnist_path, download=True, transform=transform_mnist)
 
     svhn_loader = torch.utils.data.DataLoader(dataset=svhn,
                                               batch_size=config.batch_size,
